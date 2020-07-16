5593f115e7d0e2db90656da4717a85fd87d03e36
robertgutierrrez1 <rcgr197@gmail.com>
2020-07-15 21:37:43 -0500

    Subiendo la carpeta de ejemplo

---
+++ repasojs/repasojs/js/app.js
@@ -0,0 +1,22 @@
+// Scope
+
+// var musica = 'Rock';
+
+// if(musica) {
+//     var musica = 'Grunge'
+//     console.log('dentro del if: ', musica);
+// }
+// console.log('Fuera del If', musica);
+
+
+// scope con let
+
+const musica = 'Rock';
+
+if(musica) {
+    const musica = 'Grunge'
+    console.log('dentro del if: ', musica);
+}
+console.log('Fuera del IF', musica);
+
+
---
+++ repasojs/repasojs/js/variables_en.js
@@ -0,0 +1,23 @@
+// crear variables
+
+//variables con var
+//var aprendiendo = 'JavaScript';
+
+//aprendiendo = true
+
+
+// variables con sont
+
+//const aprendiendo = 'JavaScript';
+//aprendiendo = true;
+
+//console.log(aprendiendo);
+
+
+
+let aprendiendo = 'JavaScript';
+aprendiendo = true;
+
+
+console.log(aprendiendo);
+
