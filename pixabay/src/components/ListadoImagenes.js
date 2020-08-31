621dcb71c63bb62ab793bede1564579dc36519bd
robertgutierrrez1 <rcgr197@gmail.com>
2020-08-31 15:04:19 -0500

    to pc

---
+++ pixabay/src/components/ListadoImagenes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Imagen from './Imagen';
+
+const ListadoImagenes = ({imagenes}) => {
+    return ( 
+        <div className="col-12 p-5 row">
+            {imagenes.map(imagen => (
+                <Imagen
+                    key={imagen.id}
+                    imagen={imagen}
+                />
+            ))}
+        </div>
+     );
+}
+
+Imagen.propTypes = {
+    imagenes: PropTypes.array.isRequired
+}
+
+export default ListadoImagenes;
